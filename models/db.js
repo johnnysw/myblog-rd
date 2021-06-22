690ecca5a3b019fa1d79719867ffa174227b708d
shanwei <shanwei@yeah.net>
2021-06-22 18:20:30 +0800

    修改api接口url

--- models/db.js
+++ models/db.js
@@ -13,7 +13,7 @@ var pool = mysql.createPool({
   connectionLimit: 10,
   host: "localhost",
   user: "root",
-  password: "!",
+  password: "",
   database: "myblog",
 });
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('koa-router')()
 const user = require('./users')
 const blog = require('./blogs')
 
-router.use('/user', user.routes(), user.allowedMethods())
-router.use('/blog', blog.routes(), blog.allowedMethods())
+router.use('/api/user', user.routes(), user.allowedMethods())
+router.use('/api/blog', blog.routes(), blog.allowedMethods())
 
 module.exports = router
